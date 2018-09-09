49fbee35e4d46a2a81c332b358699a6f2dc34e0e
Lucas Moreira <lucasgbd@hotmail.com.br>
2018-09-09 18:08:50 +0100

    Update

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -5,18 +5,30 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._negociacoes = new Negociacoes();
+        this._negociacoesView = new NegociacoesView('#negociacoes');
+        this._negociacoesView.update();
     }
 
     adiciona(event) {
         event.preventDefault();
 
-        let negociacao = new Negociacao(
+        this._negociacoes.adiciona(this._criaNegociacao());
+        this._limpaFormulario();
+    }
+
+    _limpaFormulario()	{
+        this._inputData.value = '';
+        this._inputQuantidade.value	= 1;
+        this._inputValor.value	= 0.0
+        this._inputData.focus();
+    }
+
+    _criaNegociacao() {
+        return new Negociacao(
             DateConverter.paraData(this._inputData.value),
             parseInt(this._inputQuantidade.value),
-            parseFloat(this._inputValor.value)
+            parseFloat(this._inputValor)
         );
-        
-        console.log(DateConverter.paraTexto(negociacao.data));
-        console.log(negociacao);
     }
 }
